30c1436e26a04af354b2cde5149d5df3dc4d3670
Colleen <colleenhiggins87@gmail.com>
2019-07-18 11:32:52 -0700

    readme

--- test/app.test.js
+++ test/app.test.js
@@ -3,9 +3,11 @@
 const app = require('../src/app')
 
 describe('App', ()=>{
+  console.log('eer')
   it('should return 200 "hello world!"', ()=>{
     return supertest(app)
     .get('/')
     .expect(200, 'hello, world!')
+    
   })
 })
